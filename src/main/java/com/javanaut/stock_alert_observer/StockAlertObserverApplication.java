8669890e265ff99790f8e1edb06b9ec0373076e7
Bibhuti Bhusan Pani <bapu@Bibhutis-MacBook-Air.local>
2024-10-30 23:42:35 +0530

    Pushing empty project to start

---
+++ src/main/java/com/javanaut/stock_alert_observer/StockAlertObserverApplication.java
@@ -0,0 +1,13 @@
+package com.javanaut.stock_alert_observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAlertObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAlertObserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanaut/stock_alert_observer/StockAlertObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanaut.stock_alert_observer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAlertObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
